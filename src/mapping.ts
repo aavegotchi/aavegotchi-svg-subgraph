192f54ea9c8a09a5810fedf2cea853b099cdc979
Frank <frank.pfeift@protonmail.com>
2021-11-22 14:48:20 +0100

    batch update sideviews in different blocks

--- src/mapping.ts
+++ src/mapping.ts
@@ -240,15 +240,78 @@ export function handleWearableSlotPositionsSet(
 export function handleMintPortals(event: MintPortals): void {}
 
 export function handleBlock(block: ethereum.Block): void {
-  if(block.number != BLOCK_SIDEVIEWS_ACTIVATED) {
-    return;
+  if(block.number == BLOCK_SIDEVIEWS_ACTIVATED) {
+    for(let i=0; i<=2500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(1))) {
+    for(let i=2500; i<=5000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(2))) {
+    for(let i=5000; i<=7500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(3))) {
+    for(let i=7500; i<=10000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(4))) {
+    for(let i=10000; i<=12500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(5))) {
+    for(let i=12500; i<=15000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(6))) {
+    for(let i=15000; i<=17500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(7))) {
+    for(let i=17500; i<=20000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(8))) {
+    for(let i=20000; i<=22500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
+  } else if(block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(9))) {
+    for(let i=22500; i<=25000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if(gotchi != null) {
+        gotchi.save();
+      }
+    } 
   }
 
   // update side views
-  for(let i=0; i<=25000; i++) {
-    let gotchi = updateSideViews(BigInt.fromI32(i));
-    if(gotchi != null) {
-      gotchi.save();
-    }
-  } 
+  
 }
