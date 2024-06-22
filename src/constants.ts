5f1078d15a2f81502bd3377807a565c7a2021d6c
orionstardust <justinono1990@outlook.com>
2024-06-22 15:11:05 -0400

    update and add amoy configuration

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
-export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(39130900);
+// matic
+// export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(39130900);
+// export const AAVEGOTCHI_DIAMOND = "0x86935F11C86623deC8a25696E1C19a8659CbF95d"
+
+// amoy
+export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(8159213);
+export const AAVEGOTCHI_DIAMOND = "0xd4275abb81e8D30A42B69A588634570e56D25ada"
