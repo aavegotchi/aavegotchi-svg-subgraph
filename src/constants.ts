947bbfcac41900c34e36a8648183b2758402a2d9
Coderdan <dmathieuva@gmail.com>
2024-11-11 13:05:32 +0800

    chore: add in vars

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,3 @@ import { BigInt } from "@graphprotocol/graph-ts";
 
 // matic
 export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(39130900);
-export const AAVEGOTCHI_DIAMOND = "0x86935F11C86623deC8a25696E1C19a8659CbF95d"
-
-// amoy
-// export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(9672225);
-// export const AAVEGOTCHI_DIAMOND = "0xC80DB01aeDAD5F6E3088c75F60E52f579Cf1D3Cb"
--- src/helper.ts
+++ src/helper.ts
@@ -1,41 +1,44 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { Contract } from "../generated/Contract/Contract";
 import { Aavegotchi } from "../generated/schema";
-import {AAVEGOTCHI_DIAMOND} from "./constants";
 
 export function updateSvg(gotchi: BigInt): Aavegotchi | null {
-    let contract = Contract.bind(Address.fromString(AAVEGOTCHI_DIAMOND))
-    let svg = contract.try_getAavegotchiSvg(gotchi);   
-    if(svg.reverted) {
-        return null; // just skip
-    }
+  let contract = Contract.bind(
+    Address.fromString(process.env.AAVEGOTCHI_DIAMOND as string)
+  );
+  let svg = contract.try_getAavegotchiSvg(gotchi);
+  if (svg.reverted) {
+    return null; // just skip
+  }
 
-    let gotchiEntity = getOrCreateAavegotchi(gotchi);
-    gotchiEntity.svg = svg.value;
-    return gotchiEntity;
+  let gotchiEntity = getOrCreateAavegotchi(gotchi);
+  gotchiEntity.svg = svg.value;
+  return gotchiEntity;
 }
 
 export function updateSideViews(gotchi: BigInt): Aavegotchi | null {
-    let contract = Contract.bind(Address.fromString(AAVEGOTCHI_DIAMOND))
-    let svgs = contract.try_getAavegotchiSideSvgs(gotchi);   
+  let contract = Contract.bind(
+    Address.fromString(process.env.AAVEGOTCHI_DIAMOND as string)
+  );
+  let svgs = contract.try_getAavegotchiSideSvgs(gotchi);
 
-    if(svgs.reverted) {
-        return null; // just skip
-    }   
-    let gotchiEntity = getOrCreateAavegotchi(gotchi);
-    let svgsValue = svgs.value;
-    gotchiEntity.svg = svgsValue[0];
-    gotchiEntity.left = svgsValue[1];
-    gotchiEntity.right = svgsValue[2];
-    gotchiEntity.back = svgsValue[3];
-    return gotchiEntity
+  if (svgs.reverted) {
+    return null; // just skip
+  }
+  let gotchiEntity = getOrCreateAavegotchi(gotchi);
+  let svgsValue = svgs.value;
+  gotchiEntity.svg = svgsValue[0];
+  gotchiEntity.left = svgsValue[1];
+  gotchiEntity.right = svgsValue[2];
+  gotchiEntity.back = svgsValue[3];
+  return gotchiEntity;
 }
 
 export function getOrCreateAavegotchi(gotchi: BigInt): Aavegotchi {
-    let gotchiEntity = Aavegotchi.load(gotchi.toString())
-    if(!gotchiEntity) {
-        gotchiEntity = new Aavegotchi(gotchi.toString())
-    }
+  let gotchiEntity = Aavegotchi.load(gotchi.toString());
+  if (!gotchiEntity) {
+    gotchiEntity = new Aavegotchi(gotchi.toString());
+  }
 
-    return gotchiEntity as Aavegotchi;
+  return gotchiEntity as Aavegotchi;
 }
