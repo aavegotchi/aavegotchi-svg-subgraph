54cbd302c7630030a3d261cdfe6dc40c739a6fc4
Coderdan <dmathieuva@gmail.com>
2024-11-25 16:44:54 +0800

    chore: update sepolia

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ import { BigInt } from "@graphprotocol/graph-ts";
 // matic
 export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(18371153);
 
-export const AAVEGOTCHI_DIAMOND = "0xb8D555680BCD42724DfdFeEa774D1C7890BE143f";
+export const AAVEGOTCHI_DIAMOND = "0xc0c35042E7CA8c6C851CC485079724eC81237Fe5";
