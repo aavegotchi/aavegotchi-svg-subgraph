70520dd9b5d103877f28e70df843755d62dfee74
Coderdan <dmathieuva@gmail.com>
2024-11-26 15:23:00 +0800

    chore: update polter

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ import { BigInt } from "@graphprotocol/graph-ts";
 // matic
 export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(18371153);
 
-export const AAVEGOTCHI_DIAMOND = "0xf81FFb9E2a72574d3C4Cf4E293D4Fec4A708F2B1";
+export const AAVEGOTCHI_DIAMOND = "0x226625C1B1174e7BaaE8cDC0432Db0e2ED83b7Ba";
