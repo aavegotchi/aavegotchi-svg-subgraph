8d02ebbe17e3398f8b229eaa27d5ae7ec9f3e17b
Coderdan <dmathieuva@gmail.com>
2024-11-28 15:39:52 +0800

    chore: add geist

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 // matic
-export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(18371153);
+export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(4273);
 
-export const AAVEGOTCHI_DIAMOND = "0x226625C1B1174e7BaaE8cDC0432Db0e2ED83b7Ba";
+export const AAVEGOTCHI_DIAMOND = "0x6Acc828BbbC6874de40Ca20bfeA7Cd2a2DA8DA8c";
