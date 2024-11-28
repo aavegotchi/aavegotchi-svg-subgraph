15471d51865fcac5770698cb90f0be9a3978e546
Coderdan <dmathieuva@gmail.com>
2024-11-28 15:52:15 +0800

    chore: update start block

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 // matic
-export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(4273);
+export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(1741966);
 
 export const AAVEGOTCHI_DIAMOND = "0x6Acc828BbbC6874de40Ca20bfeA7Cd2a2DA8DA8c";
