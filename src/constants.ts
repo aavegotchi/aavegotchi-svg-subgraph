1064b3def65309a21fb4ac45f6e181c18c5c4c47
Coderdan <dmathieuva@gmail.com>
2024-11-29 21:50:35 +0800

    chore: add svg handler for bridge

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,8 @@ import { BigInt } from "@graphprotocol/graph-ts";
 
 // matic
 export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(39130900);
+
+export const AAVEGOTCHI_DIAMOND = "0x6Acc828BbbC6874de40Ca20bfeA7Cd2a2DA8DA8c";
+
+export const AAVEGOTCHI_BRIDGE_VAULT =
+  "0xF1D1d61EEDDa7a10b494aF7af87D932AC910f3C5";
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,117 +1,133 @@
-import { BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
 import {
-    ClaimAavegotchi,
-    EquipWearables,
+  ClaimAavegotchi,
+  EquipWearables,
+  Transfer,
 } from "../generated/Contract/Contract";
-import { BLOCK_SIDEVIEWS_ACTIVATED } from "./constants";
+import {
+  AAVEGOTCHI_BRIDGE_VAULT,
+  BLOCK_SIDEVIEWS_ACTIVATED,
+} from "./constants";
 import { updateSideViews, updateSvg } from "./helper";
 
 export function handleClaimAavegotchi(event: ClaimAavegotchi): void {
-    let gotchi = event.block.number.ge(BLOCK_SIDEVIEWS_ACTIVATED)
-        ? updateSideViews(event.params._tokenId)
-        : updateSvg(event.params._tokenId);
-    if (gotchi != null) {
-        gotchi.save();
-    }
+  let gotchi = event.block.number.ge(BLOCK_SIDEVIEWS_ACTIVATED)
+    ? updateSideViews(event.params._tokenId)
+    : updateSvg(event.params._tokenId);
+  if (gotchi != null) {
+    gotchi.save();
+  }
 }
 
 export function handleEquipWearables(event: EquipWearables): void {
-    let gotchi = event.block.number.ge(BLOCK_SIDEVIEWS_ACTIVATED)
-        ? updateSideViews(event.params._tokenId)
-        : updateSvg(event.params._tokenId);
-    if (gotchi != null) {
-        gotchi.save();
-    }
+  let gotchi = event.block.number.ge(BLOCK_SIDEVIEWS_ACTIVATED)
+    ? updateSideViews(event.params._tokenId)
+    : updateSvg(event.params._tokenId);
+  if (gotchi != null) {
+    gotchi.save();
+  }
 }
 
 export function handleBlock(block: ethereum.Block): void {
-    if (block.number == BLOCK_SIDEVIEWS_ACTIVATED) {
-        for (let i = 0; i <= 2500; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(1))
-    ) {
-        for (let i = 2500; i <= 5000; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(2))
-    ) {
-        for (let i = 5000; i <= 7500; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(3))
-    ) {
-        for (let i = 7500; i <= 10000; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(4))
-    ) {
-        for (let i = 10000; i <= 12500; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(5))
-    ) {
-        for (let i = 12500; i <= 15000; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(6))
-    ) {
-        for (let i = 15000; i <= 17500; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(7))
-    ) {
-        for (let i = 17500; i <= 20000; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(8))
-    ) {
-        for (let i = 20000; i <= 22500; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
-    } else if (
-        block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(9))
-    ) {
-        for (let i = 22500; i <= 25000; i++) {
-            let gotchi = updateSideViews(BigInt.fromI32(i));
-            if (gotchi != null) {
-                gotchi.save();
-            }
-        }
+  if (block.number == BLOCK_SIDEVIEWS_ACTIVATED) {
+    for (let i = 0; i <= 2500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
     }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(1))
+  ) {
+    for (let i = 2500; i <= 5000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(2))
+  ) {
+    for (let i = 5000; i <= 7500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(3))
+  ) {
+    for (let i = 7500; i <= 10000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(4))
+  ) {
+    for (let i = 10000; i <= 12500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(5))
+  ) {
+    for (let i = 12500; i <= 15000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(6))
+  ) {
+    for (let i = 15000; i <= 17500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(7))
+  ) {
+    for (let i = 17500; i <= 20000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(8))
+  ) {
+    for (let i = 20000; i <= 22500; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  } else if (
+    block.number == BLOCK_SIDEVIEWS_ACTIVATED.plus(BigInt.fromI32(9))
+  ) {
+    for (let i = 22500; i <= 25000; i++) {
+      let gotchi = updateSideViews(BigInt.fromI32(i));
+      if (gotchi != null) {
+        gotchi.save();
+      }
+    }
+  }
+}
+
+export function handleTransfer(event: Transfer): void {
+  if (event.params._from == Address.fromString(AAVEGOTCHI_BRIDGE_VAULT)) {
+    //  - if from is bridge vault, update svg because the equipped wearables may have changed
+    let gotchi = event.block.number.ge(BLOCK_SIDEVIEWS_ACTIVATED)
+      ? updateSideViews(event.params._tokenId)
+      : updateSvg(event.params._tokenId);
+    if (gotchi != null) {
+      gotchi.save();
+    }
+  }
 }
