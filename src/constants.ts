12838e91a4ee506c5ecfb32d09652da0e0a96691
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2025-06-18 17:42:39 +0200

    fix: update contract

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ import { BigInt } from "@graphprotocol/graph-ts";
 // export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(39130900);
 
 // base-sepolia
-export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(26415768);
+export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(27243836);
 
 export const AAVEGOTCHI_BRIDGE_VAULT_MATIC =
   "0xF1D1d61EEDDa7a10b494aF7af87D932AC910f3C5";
