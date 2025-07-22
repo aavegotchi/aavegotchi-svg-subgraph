454553c03e0e8e8ce21e9b2d9a2a7016d8528516
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2025-07-22 17:51:24 +0100

    feat: add base

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,10 @@ import { BigInt } from "@graphprotocol/graph-ts";
 // export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(39130900);
 
 // base-sepolia
-export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(27243836);
+// export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(27243836);
+
+// base
+export let BLOCK_SIDEVIEWS_ACTIVATED = BigInt.fromI32(33201946);
 
 export const AAVEGOTCHI_BRIDGE_VAULT_MATIC =
   "0xF1D1d61EEDDa7a10b494aF7af87D932AC910f3C5";
